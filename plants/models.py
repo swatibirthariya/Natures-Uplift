3af154cb640bea6b5e257b13e2d43d30800cb5af
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2025-12-14 21:51:44 +0530

    Update models.py

--- plants/models.py
+++ plants/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Plant(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     size = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    image = models.ImageField(upload_to='plants/', blank=True, null=True)
+    image = models.CloudinaryField(upload_to='plants/', blank=True, null=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
