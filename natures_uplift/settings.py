e4a2918c69dfbd41ffafcbab2d0c195f7c72825f
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2025-12-14 22:19:58 +0530

    Update settings.py

--- natures_uplift/settings.py
+++ natures_uplift/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "cloudinary",
+    "cloudinary_storage",
+
     "plants",
 ]
 
