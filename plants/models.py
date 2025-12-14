d78ac92206d16675e9caa8edd8484408c572217e
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2025-12-14 22:00:37 +0530

    Update models.py

--- plants/models.py
+++ plants/models.py
@@ -6,7 +6,7 @@ class Plant(models.Model):
     description = models.TextField(blank=True)
     size = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    image = models.CloudinaryField(upload_to='plants/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
