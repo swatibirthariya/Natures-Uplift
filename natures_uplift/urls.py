dec6a9af0a4710fcf3272001763572fe7e05a684
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2026-01-02 22:50:15 +0530

    Update urls.py

--- natures_uplift/urls.py
+++ natures_uplift/urls.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('plants.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('payments/', include('payments.urls')),
+    path("cart/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
