f9870e5a17c530eafb435637f4e2b40757baa910
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2026-01-09 01:55:15 +0530

    Add files via upload

--- plants/context_processors.py
+++ plants/context_processors.py
@@ -1,5 +1,15 @@
+from accounts.models import Cart
+from django.db.models import Sum
+
 def cart_count(request):
-    cart = request.session.get('cart', {})
-    return {
-        'cart_count': sum(cart.values())
-    }
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user).first()
+        if cart:
+            total = cart.items.aggregate(
+                total=Sum('quantity')
+            )['total']
+            return {'cart_count': total or 0}
+        return {'cart_count': 0}
+    else:
+        cart = request.session.get('cart', {})
+        return {'cart_count': sum(cart.values())}
