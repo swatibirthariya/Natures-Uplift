f49f3dce2cc34323d548eb896fd95f36ddd6c5b5
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2026-01-11 19:22:59 +0530

    Delete offers/offer_tags.py

--- offers/offer_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from offers.offer_engine import get_active_offer as engine_get_offer
-
-register = template.Library()
-
-@register.simple_tag
-def active_offer():
-    return engine_get_offer()
