7ee6b6fefaee704f6b023b19f54ec9139027def1
Swati Birthariya <62589283+swatibirthariya@users.noreply.github.com>
2026-03-21 13:34:19 +0530

    Update urls.py

--- natures_uplift/urls.py
+++ natures_uplift/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from accounts.views import ping
 
 
 urlpatterns = [
+    path('ping/', ping),
     path('admin/', admin.site.urls),
 
     # plants app
